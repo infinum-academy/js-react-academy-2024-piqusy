7536f48a11f93bea4417bb4767b2495bad929738
Ivan Ramljak <ivan.ramljak@infinum.com>
2024-07-08 00:32:07 +0200

    Create ReviewForm component + style it

---
+++ movie-reviews/src/app/components/features/reviews/ReviewForm.tsx
@@ -0,0 +1,44 @@
+import { Button, Card, CardBody, FormControl, FormLabel, Heading, Radio, RadioGroup, Stack, Textarea } from '@chakra-ui/react';
+import { useState } from 'react';
+
+export const ReviewForm = () => {
+  const [value, setValue] = useState('1');
+
+  function addShowReview() {
+    console.log('hi');
+  }
+
+  return (
+    <Card w="100%">
+      <CardBody>
+        <Heading as="h2" size="lg">
+          Submit review
+        </Heading>
+
+        <FormControl>
+          <FormLabel marginTop={8} htmlFor="comment">
+            Comment
+          </FormLabel>
+          <Textarea placeholder="Add comment" id="comment" name="comment" />
+
+          <FormLabel marginTop={8} htmlFor="rating">
+            Rating
+          </FormLabel>
+          <RadioGroup onChange={setValue} value={value} name="rating">
+            <Stack direction="row">
+              <Radio value="1">1</Radio>
+              <Radio value="2">2</Radio>
+              <Radio value="3">3</Radio>
+              <Radio value="4">4</Radio>
+              <Radio value="5">5</Radio>
+            </Stack>
+          </RadioGroup>
+
+          <Button onClick={addShowReview as any} marginTop={8}>
+            Add revirew
+          </Button>
+        </FormControl>
+      </CardBody>
+    </Card>
+  );
+};
