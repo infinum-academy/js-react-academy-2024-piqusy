efeac4b94a1523182bb9ca76f07ec3e08b7917ea
Ivan Ramljak <ivan.ramljak@infinum.com>
2024-07-07 23:25:34 +0200

    Create ReviewList component and its interface + style it

--- movie-reviews/src/app/page.tsx
+++ movie-reviews/src/app/page.tsx
@@ -4,12 +4,19 @@ import { Heading } from '@chakra-ui/react';
 import { ShowDetails } from './components/features/shows/ShowDetails';
 import styles from './page.module.css';
 import '/styles/global.css';
+import { ReviewList } from './components/features/reviews/ReviewList';
+import { ReviewContainer } from './components/features/reviews/ReviewContainer';
 
 export default function Home() {
+  const reviews = ReviewContainer;
+
   return (
     <main className={styles.main}>
-      <Heading as="h1" size="2xl" color="white">Movie Reviews</Heading>
+      <Heading as="h1" size="2xl" color="white">
+        Movie Reviews
+      </Heading>
       <ShowDetails />
+      <ReviewList reviews={reviews} />
     </main>
   );
 }
--- movie-reviews/src/typings/review.ts
+++ movie-reviews/src/typings/review.ts
@@ -1,3 +1,7 @@
+interface IReviews {
+  reviews: IReview[];
+}
+
 interface IReview {
   email?: string;
   avatar?: string | undefined;
