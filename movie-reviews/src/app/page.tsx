583a71bf94a1e485b0b14fb6b68bcb4a7c44dc76
Ivan Ramljak <ivan.ramljak@infinum.com>
2024-07-08 00:35:50 +0200

    Add ShowReviewSection to page.tsx

--- movie-reviews/src/app/page.tsx
+++ movie-reviews/src/app/page.tsx
@@ -2,21 +2,18 @@
 
 import { Heading } from '@chakra-ui/react';
 import { ShowDetails } from './components/features/shows/ShowDetails';
+import { ShowReviewSection } from './components/features/shows/ShowReviewSection';
 import styles from './page.module.css';
 import '/styles/global.css';
-import { ReviewList } from './components/features/reviews/ReviewList';
-import { ReviewContainer } from './components/features/reviews/ReviewContainer';
 
 export default function Home() {
-  const reviews = ReviewContainer;
-
   return (
     <main className={styles.main}>
       <Heading as="h1" size="2xl" color="white">
         Movie Reviews
       </Heading>
       <ShowDetails />
-      <ReviewList reviews={reviews} />
+      <ShowReviewSection />
     </main>
   );
 }
