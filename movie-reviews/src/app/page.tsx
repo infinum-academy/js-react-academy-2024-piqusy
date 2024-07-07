486f875480c747b577893a1531c443ac65bb9649
Ivan Ramljak <ivan.ramljak@infinum.com>
2024-07-07 18:01:55 +0200

    Init Next.js React app, add and setup Chakra UI

---
+++ movie-reviews/src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Movie Reviews</h1>
+    </main>
+  );
+}
