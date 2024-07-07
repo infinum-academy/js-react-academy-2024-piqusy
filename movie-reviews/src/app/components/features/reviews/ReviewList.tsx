f11a0025fcbe64e9182d9e8644cefca1bd448a3a
Ivan Ramljak <ivan.ramljak@infinum.com>
2024-07-07 23:49:04 +0200

    Improve general layout with gap property on .main

--- movie-reviews/src/app/components/features/reviews/ReviewList.tsx
+++ movie-reviews/src/app/components/features/reviews/ReviewList.tsx
@@ -5,7 +5,7 @@ export const ReviewList = ({ reviews }: IReviews) => {
   const reviewList = reviews.map((review) => <ReviewItem key={review.email} {...review} />);
 
   return (
-    <Stack marginTop={10} spacing={4} w="100%">
+    <Stack spacing={4} w="100%">
       <Heading color="white">Review List</Heading>
       <Stack spacing={4} w="100%">
         {reviewList}
