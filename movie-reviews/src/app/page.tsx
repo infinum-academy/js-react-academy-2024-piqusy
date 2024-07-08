3006230799bbab31221d1733548bd709b875656f
Ivan Ramljak <ivan.ramljak@infinum.com>
2024-07-08 23:24:39 +0200

    Replace HMTL tags with Chakra components

--- movie-reviews/src/app/page.tsx
+++ movie-reviews/src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Heading } from '@chakra-ui/react';
+import { Box, Container, Heading, VStack } from '@chakra-ui/react';
 import { ShowDetails } from './components/features/shows/ShowDetails';
 import { ShowReviewSection } from './components/features/shows/ShowReviewSection';
 import styles from './page.module.css';
@@ -8,12 +8,16 @@ import '/styles/global.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Heading as="h1" size="2xl" color="white">
-        Movie Reviews
-      </Heading>
-      <ShowDetails />
-      <ShowReviewSection />
-    </main>
+    <Box bg='black'>
+      <Container paddingBlock={20}>
+        <VStack spacing={10}>
+          <Heading as="h1" size="2xl" color="white">
+            Movie Reviews
+          </Heading>
+          <ShowDetails />
+          <ShowReviewSection />
+        </VStack>
+      </Container>
+    </Box>
   );
 }
