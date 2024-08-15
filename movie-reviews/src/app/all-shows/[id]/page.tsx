193c91b94f47929d5acd55c73c5cff0632d941d7
Ivan Ramljak <ivan.ramljak@infinum.com>
2024-08-16 01:08:00 +0200

    Implement all-shows/id page

---
+++ movie-reviews/src/app/all-shows/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { ShowDetailsSection } from "@/app/components/features/show/ShowDetailsSection/ShowDetailsSection";
+
+export default function ShowDetailsPage() {
+	return <ShowDetailsSection />;
+}
